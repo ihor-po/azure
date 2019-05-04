8fdaa38422cbd320efa6385c5e9d551229c65786
Ihor Porotikov <sobikashi@gmail.com>
2019-05-04 14:28:59 +0300

    add uri generator

--- BlobAccess/Program.cs
+++ BlobAccess/Program.cs
@@ -30,6 +30,7 @@ namespace BlobAccess
 
                 Console.WriteLine(@"Getting reference to conteiner");
                 CloudBlobContainer container = blobClient.GetContainerReference(downloadContainer);
+                
 
                 // If container not exist - create container in blob storage
                 container.CreateIfNotExists();
@@ -38,12 +39,14 @@ namespace BlobAccess
                 string[] fileEntries = Directory.GetFiles(localFolder);
                 if (fileEntries.Count() > 0)
                 {
+                    Console.WriteLine("Uploading files..." + Environment.NewLine);
+
                     foreach (string filePath in fileEntries)
                     {
                         //Create new file path for file
                         string key = DateTime.UtcNow.ToString("yyyy-MM-dd-HH_mm_ss") + "-" + Path.GetFileName(filePath);
 
-                        UploadBlob(container, key, filePath, true);
+                        UploadBlob(container, key, filePath, false);
                     }
                 }
                 
@@ -51,7 +54,9 @@ namespace BlobAccess
 
             } catch(Exception ex)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
             }
 
             Console.ReadKey();
@@ -64,32 +69,38 @@ namespace BlobAccess
         /// <param name="key">File path</param>
         /// <param name="fileName">File name in storage</param>
         /// <param name="deleteAfter">If need delete from folder after uploading</param>
-        private async static void UploadBlob(CloudBlobContainer container, string key, string fileName, bool deleteAfter)
+        /// <param name="maxAge">If need cach controle set string like max-age=3600</param>
+        private async static void UploadBlob(CloudBlobContainer container, string key, string fileName, bool deleteAfter, string maxAge = "")
         {
-            try
+            // Retrieving files not for uploading
+            string[] fExeptions = fileName.Split('\\').Where(item => item == ".gitkeep").ToArray();
+
+            if (fExeptions.Count() == 0)
             {
-                Console.WriteLine(@"Uploading file to container: key=" + key + " source file=" + fileName);
-
-                //Get reference to blob object in BLOB container
-                CloudBlockBlob blob = container.GetBlockBlobReference($"{DateTime.UtcNow.ToString("yyyy-MM-dd")}/{key}");
-
-                //Saving file to blob
-                using (var fs = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.None))
+                try
                 {
-                    await blob.UploadFromStreamAsync(fs);
+                    //Get reference to blob object in BLOB container
+                    CloudBlockBlob blob = container.GetBlockBlobReference($"{DateTime.UtcNow.ToString("yyyy-MM-dd")}/{key}");
+
+                    //Saving file to blob
+                    using (var fs = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.None))
+                    {
+                        await blob.UploadFromStreamAsync(fs);
+                    }
+;
+                    ShowMessage("File Uri for Reading: " + Environment.NewLine + CreateFileUri(blob));
+
+                    //Deleting file from local folder
+                    if (deleteAfter)
+                    {
+                        File.Delete(fileName);
+                    }
                 }
-
-                //Deleting file from local folder
-                if (deleteAfter && fileName != ".gitkeep")
+                catch (Exception ex)
                 {
-                    File.Delete(fileName);
+                    ShowMessage(ex.Message, 0);
                 }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            
         }
 
         /// <summary>
@@ -111,6 +122,7 @@ namespace BlobAccess
                     {
                         var dir = dirItem as CloudBlobDirectory;
                         Console.WriteLine($"Start download directory:\n{dir.Prefix}");
+                        Console.WriteLine(Environment.NewLine);
                         
                         BlobContinuationToken blobToken = null;
                         var blobResult = await dir.ListBlobsSegmentedAsync(blobToken);
@@ -119,22 +131,87 @@ namespace BlobAccess
                         {
                             if (blobItem is CloudBlockBlob)
                             {
-                                var blob = blobItem as CloudBlockBlob;
-
-                                string fileName = blob.Name;
-                                fileName = fileName.Replace("/", "\\");
-
-                                string folderName = fileName.Split('\\')[0];
-                                Directory.CreateDirectory($"{localFolder}\\{folderName}");
-
-                                Console.WriteLine($"Download file: {fileName} from blob");
-
-                                blob.DownloadToFile($"{localFolder}/{fileName}", FileMode.Create);
+                                DownloadFileFromBlob(blobItem as CloudBlockBlob, localFolder);
                             }
                         }
+                        ShowMessage("\tDownload succefull!", 1);
                     }
                 }
             } while (dirToken != null);
+        }
+
+        /// <summary>
+        /// Download file from Blob container
+        /// </summary>
+        /// <param name="blob"></param>
+        /// <param name="pathTo"></param>
+        private async static void DownloadFileFromBlob(CloudBlockBlob blob, string pathTo)
+        {
+            try
+            {
+                blob.Properties.CacheControl = (3600 * 24 * 7).ToString();
+
+                string fileName = blob.Name;
+                fileName = fileName.Replace("/", "\\");
+
+                string folderName = fileName.Split('\\')[0];
+                Directory.CreateDirectory($"{pathTo}\\{folderName}");
+
+                Console.WriteLine($"\tDownload file: {fileName}");
+
+               await blob.DownloadToFileAsync($"{pathTo}/{fileName}", FileMode.Create);
+            } catch(Exception ex)
+            {
+                ShowMessage("ERROR: " + ex.Message, 0);
+            }
+        }
+
+        /// <summary>
+        /// Create file uri for reading
+        /// </summary>
+        /// <param name="blob"></param>
+        /// <returns></returns>
+        private static string CreateFileUri(CloudBlockBlob blob)
+        {
+            //Create contrains for uri
+            SharedAccessBlobPolicy constraints = new SharedAccessBlobPolicy();
+            
+            //Time expired
+            constraints.SharedAccessExpiryTime = DateTimeOffset.UtcNow.AddDays(7);
+
+            //Create permission for only reading
+            constraints.Permissions = SharedAccessBlobPermissions.Read;
+
+            //Generate token
+            string token = blob.GetSharedAccessSignature(constraints);
+
+            //Return uri with token
+            return blob.Uri + token;
+        }
+
+        /// <summary>
+        /// Show color message
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color">0 - red, 1 - green, default - yellow</param>
+        private static void ShowMessage(string message, short color = 2)
+        {
+            switch(color)
+            {
+                case 0:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+            }
+
+            Console.WriteLine(message);
+            Console.WriteLine(Environment.NewLine);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
